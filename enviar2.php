bd7df6da9a4344b03b8ac87272dcc17d7a0e8d11
davidrom555 <50477094+davidrom555@users.noreply.github.com>
2020-04-02 22:48:30 -0300

    Add files via upload

---
+++ enviar2.php
@@ -0,0 +1,29 @@
+<?php
+$nombre = $_POST['nombre'];
+$mail = $_POST['email'];
+$empresa = $_POST['mensaje'];
+$tel = $_POST['telefono'];
+
+
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Su telefono es: " . $tel . " \r\n";
+
+
+
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'marketecnia.info@gmail.com';
+$asunto = 'Marketecnia';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html");
+?>
